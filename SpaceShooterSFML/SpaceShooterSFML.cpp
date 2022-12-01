bbc7a23b2c4fead7bc376a2f30c55f5681d14cee
Maxens Thiam <MaxensThiam@outlook.com>
2022-12-01 14:00:33 +0100

    gitignore and updated spaceship
    added colors and more features to the spaceship

--- SpaceShooterSFML/SpaceShooterSFML.cpp
+++ SpaceShooterSFML/SpaceShooterSFML.cpp
@@ -6,18 +6,28 @@ int main()
 {
 	sf::RenderWindow window(sf::VideoMode(1920, 1080), "ChronoSpacer");
 	// Initialise everything below
-	ConvexShape convex;
-	convex.setPointCount(3);
-	convex.setPoint(0, Vector2f(0, -75));
-	convex.setPoint(1, Vector2f(-25, 25));
-	convex.setPoint(2, Vector2f(25, 25));
-	convex.setPosition(Vector2f{ window.getSize().x / 2.0f ,window.getSize().y / 2.0f });
-	ConvexShape convex2;
-	convex2.setPointCount(3);
-	convex2.setPoint(0, Vector2f(0, -20));
-	convex2.setPoint(1, Vector2f(-50, 10));
-	convex2.setPoint(2, Vector2f(50, 10));
-	convex2.setPosition(Vector2f{ window.getSize().x / 2.0f ,window.getSize().y / 2.0f + 10 });
+	ConvexShape spaceship1;
+	spaceship1.setPointCount(3);
+	spaceship1.setPoint(0, Vector2f(0, -75));
+	spaceship1.setPoint(1, Vector2f(-25, 25));
+	spaceship1.setPoint(2, Vector2f(25, 25));
+	spaceship1.setPosition(Vector2f{ window.getSize().x / 2.0f ,window.getSize().y / 2.0f });
+
+	ConvexShape spaceship2;
+	spaceship2.setPointCount(3);
+	spaceship2.setPoint(0, Vector2f(0, -20));
+	spaceship2.setPoint(1, Vector2f(-60, 10));
+	spaceship2.setPoint(2, Vector2f(60, 10));
+	spaceship2.setFillColor(Color(200, 200, 200));
+	spaceship2.setPosition(Vector2f{ window.getSize().x / 2.0f ,window.getSize().y / 2.0f + 10 });
+
+	ConvexShape spaceship3;
+	spaceship3.setPointCount(3);
+	spaceship3.setPoint(0, Vector2f(0, 20));
+	spaceship3.setPoint(1, Vector2f(-50, -5));
+	spaceship3.setPoint(2, Vector2f(50, -5));
+	spaceship3.setFillColor(Color(150, 150, 150));
+	spaceship3.setPosition(Vector2f{ window.getSize().x / 2.0f ,window.getSize().y / 2.0f + 10 });
 	// Game loop
 	while (window.isOpen()) {
 		sf::Event event;
@@ -29,8 +39,9 @@ int main()
 		}
 
 		window.clear();
-		window.draw(convex);
-		window.draw(convex2);
+		window.draw(spaceship3);
+		window.draw(spaceship2);
+		window.draw(spaceship1);
 		// Whatever I want to draw goes here
 		window.display();
 	}
