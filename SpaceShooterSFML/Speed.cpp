41e2dc95e7c99c0614c362ffcab3882282db3278
Maxens Thiam <MaxensThiam@outlook.com>
2022-12-09 16:07:37 +0100

    ship sprite

--- SpaceShooterSFML/Speed.cpp
+++ SpaceShooterSFML/Speed.cpp
@@ -20,9 +20,9 @@ void setupSpaceShip(SpaceShip& spaceShip , Vector2f spaceShipPosition, float spa
 	spaceShip.spaceship1.setPosition(spaceShipPosition);
 
 	spaceShip.spaceship2.setPointCount(4);
-	spaceShip.spaceship2.setPoint(0, Vector2f(0, -20));
+	spaceShip.spaceship2.setPoint(0, Vector2f(0, -15));
 	spaceShip.spaceship2.setPoint(1, Vector2f(-60, 15));
-	spaceShip.spaceship2.setPoint(2, Vector2f(0, 10));
+	spaceShip.spaceship2.setPoint(2, Vector2f(0, 20));
 	spaceShip.spaceship2.setPoint(3, Vector2f(60, 15));
 	spaceShip.spaceship2.setFillColor(Color(200, 200, 200));
 	spaceShip.spaceship2.setPosition(spaceShipPosition);
